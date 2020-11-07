9c8205fa2546350cabcf263fb22123cce4dca101
Victoria Landron <viclandron@gmail.com>
2020-11-07 17:35:39 -0500

    add homepage and main handlebars as well as styling for page

---
+++ controllers/home-routes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+      attributes: [
+        'id',
+        'title',
+        'content',
+        'created_at'
+        ],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        // pass a single post object into the homepage template
+        res.render('homepage', { posts });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+module.exports = router;
