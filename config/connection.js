3adde8f123d6c149a2a2987b7ad7a88afa7db890
Victoria Landron <viclandron@gmail.com>
2020-11-08 17:21:32 -0500

    fixed connection to jaws

--- config/connection.js
+++ config/connection.js
@@ -3,18 +3,12 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-let sequelize;
 
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-}
-else{
-    //connect to database
-    const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
-    });
-}
+//connect to database
+const sequelize = process.env.JAWSDB_URL? new Sequelize(process.env.JAWSDB_URL): new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+});
 
 module.exports = sequelize;
